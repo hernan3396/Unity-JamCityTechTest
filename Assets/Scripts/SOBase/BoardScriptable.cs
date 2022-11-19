d1b069025a2b96bc9a9715f3f188da876f5ea552
hernan3396 <51168950+hernan3396@users.noreply.github.com>
2022-11-19 12:28:06 -0300

    Board Scriptable created

---
+++ Assets/Scripts/SOBase/BoardScriptable.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewBoard", menuName = "JamCityTechTest/New Board", order = 1)]
+public class BoardScriptable : ScriptableObject
+{
+    public Vector2 BoardSize;
+}
